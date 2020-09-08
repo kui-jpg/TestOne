ef1a7f8b08098a4499354da39995f29c7ef9dd64
html_kui <html_kui@163.com>
2020-09-08 16:59:36 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            string MyName=string.Empty;
+            MyName = "kuikui";
+            int a = 2;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("heiheiheihei"); 
+            Console.WriteLine(a+++1);
+            Console.WriteLine(++a);
+        }
+    }
+}
