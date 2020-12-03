d70a24a3b0ade163d7c37fc793ec48c860d12ec4
html_kui <html_kui@163.com>
2020-12-03 16:09:02 +0800

    调用阿里云天气信息API

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Web;
+
 
 namespace ConsoleApp1
 {
     
     class Program
     {
-         
+
+        private const String host = "https://iweather.market.alicloudapi.com";
+        private const String path = "/address";
+        private const String method = "GET";
+        private const String appcode = "0504222bd4b34d59ac06d15baf0365ed";
         static void Main(string[] args)
         {
-            string MyName=string.Empty;
-            MyName = "kuikui";
-            int a = 2;
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("heiheiheihei"); 
-            Console.WriteLine(a+++1);
-            Console.WriteLine(++a);
+           
+            
+
+            //请求参数（Query） 必填
+            //area      市县；如西湖
+            //city      城市 ；如武汉
+            //needday   请求数据；1：实时，7：7天，15：15天，24：24小时
+            //prov      省份；如湖北
+            String querys = "area=仲恺高新区&city=惠州市&needday=1&date=202012&prov=广东省";
+            String bodys = "";
+            String url = host + path;
+            HttpWebRequest httpRequest = null;
+            HttpWebResponse httpResponse = null;
+
+            if (0 < querys.Length)
+            {
+                url = url + "?" + querys;
+            }
+
+            if (host.Contains("https://"))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                httpRequest = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+            }
+            else
+            {
+                httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            }
+            httpRequest.Method = method;
+            httpRequest.Headers.Add("Authorization", "APPCODE " + appcode);
+            if (0 < bodys.Length)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(bodys);
+                using (Stream stream = httpRequest.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            try
+            {
+                httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                httpResponse = (HttpWebResponse)ex.Response;
+            }
+
+            Console.WriteLine(httpResponse.StatusCode);
+            Console.WriteLine(httpResponse.Method);
+            Console.WriteLine(httpResponse.Headers);
+            
+            Stream st = httpResponse.GetResponseStream();
+            StreamReader reader = new StreamReader(st, Encoding.GetEncoding("utf-8"));
+            //StreamReader reader = new StreamReader(st);
+            Console.WriteLine(reader.ReadToEnd());
+            Console.WriteLine("\n");
+            
+        }
+        public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
         }
     }
 }
